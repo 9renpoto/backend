8fe8af556a0d6bf13d673bcfe8c71de4d1441ef9
Keisuke Kan <9renpoto@gmail.com>
2019-12-08 10:37:08 +0900

    chore: use fastify

---
+++ apps/me/__tests__/app.e2e-spec.ts
@@ -0,0 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import * as request from 'supertest'
+import { AppModule } from './../src/app.module'
+import { FastifyAdapter } from '@nestjs/platform-fastify'
+import { randomPort } from '../test.util'
+
+describe('AppController (e2e)', () => {
+  let app
+
+  beforeEach(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+    const port = await randomPort()
+    app = moduleFixture.createNestApplication(new FastifyAdapter())
+    await app.listen(port)
+  })
+
+  afterEach(async () => await app.close())
+
+  it('/ (GET)', async () => {
+    request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello World!')
+  })
+})
