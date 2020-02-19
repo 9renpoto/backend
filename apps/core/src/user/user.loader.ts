25e4b9303c06bd627b26c2871fd8bfa5e5ccf1a1
Keisuke Kan <9renpoto@gmail.com>
2020-02-19 17:18:25 +0900

    chore: update specs

--- apps/core/src/user/user.loader.ts
+++ apps/core/src/user/user.loader.ts
@@ -1,16 +1,18 @@
-import DataLoader from 'dataloader';
-import { Injectable } from '@nestjs/common';
-import { NestDataLoader } from 'nestjs-dataloader';
-import { UserService } from './user.service';
-import { User } from './user.entity';
+import DataLoader from 'dataloader'
+import { Injectable } from '@nestjs/common'
+import { NestDataLoader } from 'nestjs-dataloader'
+import { UserService } from './user.service'
+import { User } from './user.entity'
 
 export type UserLoader = DataLoader<string, User>
 
 @Injectable()
 export class UserDataLoader implements NestDataLoader<string, User> {
-  constructor(private readonly service: UserService) { }
+  constructor(private readonly service: UserService) {}
 
   generateDataLoader(): UserLoader {
-    return new DataLoader<string, User>(keys => this.service.find(keys as string[]));
+    return new DataLoader<string, User>(keys =>
+      this.service.find(keys as string[])
+    )
   }
 }
--- apps/core/src/user/user.module.ts
+++ apps/core/src/user/user.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { UserService } from './user.service';
-import { User } from './user.entity';
-import { UserDataLoader } from './user.loader';
-import { UserResolver } from './user.resolver';
+import { UserService } from './user.service'
+import { User } from './user.entity'
+import { UserDataLoader } from './user.loader'
+import { UserResolver } from './user.resolver'
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
