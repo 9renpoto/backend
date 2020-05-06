22dd5d8b5aaef4656be521515da4f80d71dfb602
Keisuke Kan <9renpoto@gmail.com>
2020-05-06 23:41:29 +0900

    chore: Added apiVersion

--- apps/core/src/config/stripe.ts
+++ apps/core/src/config/stripe.ts
@@ -3,6 +3,7 @@ import { registerAs } from '@nestjs/config'
 
 const config: StripeOptions = {
   apiKey: process.env.STRIPE_KEY!,
+  apiVersion: '2019-12-03',
 }
 
 export default registerAs('stripe', () => config)
